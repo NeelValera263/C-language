a255f5a90a7582bf7630454573a7399dc572602f
NeelValera263 <neelvalera1997@gmail.com>
2026-08-12 00:06:33 +0530

    Edited the file

--- sumof2numbers.c
+++ sumof2numbers.c
@@ -7,7 +7,7 @@ int main() {
     printf("Enter your second No. : \n");
     scanf("%d", &b);
     int sum = a + b;
-    printf("%d + %d = %d",a,b,sum);
+    printf("%d + %d = %d\n",a,b,sum);
     printf("Hence the sum of 2 numbers is %d", a+b);
     return 0;
 }
